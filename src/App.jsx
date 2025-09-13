c9aeee5909db1dffcf5320e2fa5612a3c79bcc51
Sudarshan Maske <ms22maske@gmail.com>
2025-09-13 23:01:59 +0530

    Ui checkup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import SearchBar from './components/SearchBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handleSearch = (val) =>{
+    alert('search for: ' + val)
+  }
   return (
-    <>
-    </>
+     <div className="min-h-screen p-6">
+      <header className="max-w-4xl mx-auto mb-6">
+        <h1 className="text-3xl font-bold">Recipe Ideas 🍳</h1>
+        <p className="text-gray-600 mt-1">Search recipes by ingredient.</p>
+      </header>
+
+      <main className="max-w-4xl mx-auto">
+        <SearchBar onSearch={handleSearch} />
+      </main>
+    </div>
   )
 }
 
